8ca73d6b7211d82861ebe13f09eacec10eddbf46
Steelcat <steelpuppy@gmail.com>
2016-10-28 00:21:58 +0300

    01#1 extended PSR2 standart

--- 01/index.php
+++ 01/index.php
@@ -5,17 +5,16 @@ $name = 'Виталий';
 $age = 43;
 $age_second_digit = $age % 10;
 switch ($age_second_digit) {
-    case 1: {
+    case 1:
         $age_title = 'год';
         break;
-    }
-    case 2: case 3: case 4: {
+    case 2:
+    case 3:
+    case 4:
         $age_title = 'года';
         break;
-    }
-    default: {
+    default:
         $age_title = 'лет';
-    }
 }
 echo "Меня зовут: $name <br>";
 echo "Мне " . $age . " $age_title <br>";
