d0e28e98d87a70acb480e473d7bf2f9c19874552
Steelcat <steelpuppy@gmail.com>
2016-10-27 23:46:15 +0300

    #1 extended

--- 01/index.php
+++ 01/index.php
@@ -2,7 +2,21 @@
 
 // #1
 $name = 'Виталий';
-$age = 43;
+$age = 41;
+$age_second_digit = $age % 10;
+switch ($age_second_digit) {
+    case 1: {
+        $age_title = 'год';
+        break;
+    }
+    case 2: case 3: case 4: {
+        $age_title = 'года';
+        break;
+    }
+    default: {
+        $age_title = 'лет';
+    }
+}
 echo "Меня зовут: $name <br>";
-echo 'Мне ' . $age . ' лет<br>';
+echo "Мне " . $age . " $age_title <br>";
 echo '“!|\\/’”\\<br>';
