c5c3fef80177ba5039c731676991a23c3c69d883
Steelcat <steelpuppy@gmail.com>
2016-10-30 21:24:55 +0300

    01#5 + 6 complete

--- 01/index.php
+++ 01/index.php
@@ -62,6 +62,27 @@ echo "<br>";
 echo "<br>";
 
 // #5
+$day = 6;
+switch ($day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo "Это рабочий день";
+        break;
+    case 6:
+    case 7:
+        echo "Это выходной день";
+        break;
+    default:
+        echo "Неизвестный день";
+}
+echo "<br>";
+
+echo "<br>";
+
+// #6
 
 /**
  * BMW
