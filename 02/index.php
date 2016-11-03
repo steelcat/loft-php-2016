f7999d78b2b153238fc1f5ece123e44815b6310d
Steelcat <steelpuppy@gmail.com>
2016-11-03 19:11:16 +0300

    02#1

---
+++ 02/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$arr = ['Хорошая', 'погода', 'сегодня'];
+
+// #1
+/**
+ * @param array $arr
+ * @param bool $cond
+ * @return string
+ */
+function string_to_paragraph(array $arr, $cond = false)
+{
+    if ($cond) {
+        $result = implode('', $arr);
+        return $result;
+    } else {
+        $result = '';
+        foreach ($arr as $arr_item) {
+            $result = $result . "<p> $arr_item </p>";
+        }
+        echo $result;
+        return $result;
+    }
+}
+
+/**
+ * Test of string_to_paragraph function
+ */
+string_to_paragraph($arr, false);
+echo '<br>';
+echo string_to_paragraph($arr, true);
+
+echo '<br>';
