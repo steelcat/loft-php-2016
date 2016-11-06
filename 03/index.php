38bf2fbd06923bffb01ff0171660753931780535
Steelcat <steelpuppy@gmail.com>
2016-11-06 22:58:51 +0300

    03#4

---
+++ 03/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// #4
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
+curl_setopt($curl , CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = json_decode(curl_exec($curl), true);
+$page = array_values($result['query']['pages'])[0];
+$pagetitle = $page['title'];
+$pageid = $page['pageid'];
+echo "Title: $pagetitle <br>ID: $pageid <br>";
+curl_close($curl);
