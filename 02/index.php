49dd014f14fc3f6320ed4b86cd8455fab75a44ae
Steelcat <steelpuppy@gmail.com>
2016-11-06 16:03:10 +0300

    02#7

--- 02/index.php
+++ 02/index.php
@@ -179,3 +179,11 @@ echo strtotime("24.02.2016 00:00:00");
 echo '<br>';
 
 echo '<br><br>';
+
+// #7
+echo str_replace("К", "", "Карл у Клары украл Кораллы");
+echo '<br>';
+echo str_replace("Две", "Три", "Две бутылки лимонада");
+echo '<br>';
+
+echo '<br><br>';
