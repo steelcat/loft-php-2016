e6d07d6bc9ca90a2435be39dab48e98031bba940
Steelcat <steelpuppy@gmail.com>
2016-11-06 10:57:38 +0300

    02#2

--- 02/index.php
+++ 02/index.php
@@ -34,4 +34,53 @@ var_dump(string_to_paragraph($arr, true));
 echo '<br>';
 echo string_to_paragraph($arr, true);
 
+echo '<br><br>';
+
+// #2
+/**
+ * @param $digits_arr
+ * @param $arithmetic
+ */
+function arr_to_arithmetic($digits_arr, $arithmetic)
+{
+    $result = 0;
+    try {
+        foreach ($digits_arr as $digit) {
+            if (is_int($digit)) {
+                switch ($arithmetic) {
+                    case '+':
+                        $result = $result + $digit;
+                        break;
+                    case '-':
+                        $result = $result - $digit;
+                        break;
+                    case '*':
+                        $result = $result * $digit;
+                        break;
+                    case '/':
+                        $result = $result / $digit;
+                        break;
+                    default:
+                        $result = "Задана некорректная арифметическая операция";
+                }
+            } else {
+                $result = "Задано не числовое значение";
+                break;
+            }
+        }
+    } catch (Exception $e) {
+        $result = "Произошла неизвестная ошибка";
+    }
+    echo $result;
+}
+
+/**
+ * Test of arr_to_arithmetic function
+ */
+arr_to_arithmetic([1, 2, 3, 4, 5], '+'); // correct
 echo '<br>';
+arr_to_arithmetic([1, 2, false, 4, 5], '+'); // not correct, not correct digit
+echo '<br>';
+arr_to_arithmetic([1, 2, 3, 4, 5], '='); // not correct, not correct operator
+
+echo '<br><br>';
