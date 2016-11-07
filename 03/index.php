b995183d9e63d53b2402ccbb754af965bf7d5d91
Steelcat <steelpuppy@gmail.com>
2016-11-07 20:40:55 +0300

    03#1 start

--- 03/index.php
+++ 03/index.php
@@ -1,9 +1,11 @@
 <?php
+// #1
+
 
 // #4
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
-curl_setopt($curl , CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_HEADER, 0);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $result = json_decode(curl_exec($curl), true);
 $page = array_values($result['query']['pages'])[0];
