3ebd2d1f6955970aeeb25df5bb742e2d3d65db7f
Steelcat <steelpuppy@gmail.com>
2016-11-14 21:34:48 +0300

    03 SQL dump

---
+++ 03/dump.sql
@@ -0,0 +1,39 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.5.48 - MySQL Community Server (GPL)
+-- ОС Сервера:                   Win32
+-- HeidiSQL Версия:              9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дамп структуры для таблица loft-php-03.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(10) unsigned DEFAULT NULL,
+  `about` text,
+  `picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы loft-php-03.users: ~7 rows (приблизительно)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `age`, `about`, `picture`) VALUES
+	(1, 'vitaly', 'password', 'Виталик', 43, 'Обо мне', '1-582a00629f9c0.jpg'),
+	(2, 'user', 'user', NULL, NULL, NULL, NULL),
+	(3, 'new', 'newnew', 'Толик', 18, 'Толян', '3-582a00a7d399e-14791.jpg'),
+	(4, 'test', 'test', 'Test', 34, 'Ddddпвапва пвапвап', NULL),
+	(5, 'dfgdfg', 'dfgdfgd', NULL, NULL, NULL, NULL),
+	(6, 'dfgdgf', 'dfgdfg', NULL, NULL, NULL, NULL),
+	(7, '333', '333', NULL, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
