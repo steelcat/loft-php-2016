1550d60a4b187b081f28ea3ac361c8d913478dff
Steelcat <steelpuppy@gmail.com>
2016-11-16 19:27:21 +0300

    04 Готова первая часть

--- 04/index.php
+++ 04/index.php
@@ -1,17 +1,46 @@
 <?php
 // #1
 $xml_data = simplexml_load_file('data.xml');
-echo 'Purchase Order Number : ' . $xml_data["PurchaseOrderNumber"] . '<br>';
-echo 'Order Date : ' . $xml_data["OrderDate"] . '<br>';
-echo '-----<br>';
-echo 'Shipping Address : ' . $xml_data->Address[0] . '<br>';
+
+echo '<h1>Заказ номер : ' . $xml_data["PurchaseOrderNumber"] . '</h1>';
+echo 'Дата покупки : ' . $xml_data["OrderDate"];
+echo '<h2>Адреса</h2>';
+$addresses = $xml_data->Address;
+foreach ($addresses as $address) {
+    echo 'Тип адреса : ' . $address['Type'] . '<br>';
+    echo '-----<br>';
+    echo 'Имя : ' . (string) $address->Name . '<br>';
+    echo 'Улица : ' . (string) $address->Street . '<br>';
+    echo 'Город : ' . (string) $address->City . '<br>';
+    echo 'Штат : ' . (string) $address->State . '<br>';
+    echo 'Индекс : ' . (string) $address->Zip . '<br>';
+    echo 'Страна : ' . (string) $address->Country . '<br>';
+    echo '<br>';
+}
+
+echo 'Примечания к заказу : ' . (string) $xml_data->DeliveryNotes . '<br>';
+
+echo '<h2>Товары</h2>';
+
+$items = $xml_data->Items->Item;
+foreach ($items as $item) {
+    echo 'Артикул : ' . $item['PartNumber'] . '<br>';
+    echo '-----<br>';
+    echo 'Название : ' . (string) $item->ProductName . '<br>';
+    echo 'Количество : ' . (string) $item->Quantity . '<br>';
+    echo 'Цена : $' . (string) $item->USPrice . '<br>';
+    echo 'Дата доставки : ' . (string) $item->ShipDate . '<br>';
+    echo 'Комментарий : ' . (string) $item->Comment . '<br>';
+    echo '<br>';
+}
+
 
 
 echo '<br><br>';
 
 // #4
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=PHP|Main&prop=revisions&rvprop=content&format=json");
+curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
 curl_setopt($curl, CURLOPT_HEADER, 0);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $result = json_decode(curl_exec($curl), true);
