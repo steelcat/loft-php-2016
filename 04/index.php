ef2d363a4902d5c74060d89d42abb81ab9cc375b
Steelcat <steelpuppy@gmail.com>
2016-11-16 18:50:14 +0300

    04 Добавил возможность просматривать несколько страниц из запроса

--- 04/index.php
+++ 04/index.php
@@ -11,12 +11,14 @@ echo '<br><br>';
 
 // #4
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
+curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=PHP|Main&prop=revisions&rvprop=content&format=json");
 curl_setopt($curl, CURLOPT_HEADER, 0);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $result = json_decode(curl_exec($curl), true);
-$page = array_values($result['query']['pages'])[0];
-$pagetitle = $page['title'];
-$pageid = $page['pageid'];
-echo "Title: $pagetitle <br>ID: $pageid <br>";
+$pages = array_values($result['query']['pages']);
+foreach ($pages as $page) {
+    $pagetitle = $page['title'];
+    $pageid = $page['pageid'];
+    echo "Title: $pagetitle <br>ID: $pageid <br>";
+}
 curl_close($curl);
