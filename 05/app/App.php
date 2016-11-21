9cd3d548c83d7a36c4a3f7c182345d28e5655d1c
Steelcat <steelpuppy@gmail.com>
2016-11-21 10:26:41 +0300

    05 App rework

--- 05/app/App.php
+++ 05/app/App.php
@@ -1,24 +1,55 @@
 <?php
-/**
- * Project: loft-php-2016
- * Author: Zorca (vs@zorca.org)
- */
-
 namespace App;
 
 /**
- * Class App
+ * Class App (Singleton Pattern)
  * @package App
  */
 class App
 {
+    protected static $instance = null;
+
+    private static $config = null;
+    private static $auth = false;
+    private static $post = null;
+
+    protected function __construct()
+    {
+        self::$config = (file_exists(BASE . 'config.php')) ? require BASE . 'config.php' : null;
+        self::$auth = (bool) Session::get('id');
+        self::$post = $_POST;
+    }
+
+    public static function getConfig($key)
+    {
+        if (!empty(self::$config[$key])) {
+            return self::$config[$key];
+        }
+        return null;
+    }
+
+    public static function isAuth()
+    {
+        return self::$auth;
+    }
+
+    public static function getPost($key)
+    {
+        if (!empty(self::$post[$key])) {
+            return self::$post[$key];
+        }
+        return null;
+    }
+
     public static function run()
     {
-        $config = Config::get();
-        $router = Router::get($config);
-        $controllerFullName = '\\App\\Controllers\\' . $router['controllerName'];
-        $controller = $controllerFullName::run();
-        //$model = $controller['modelName']::run($controller);
-        View::show($router);
+        if (!isset(self::$instance)) {
+            self::$instance = new App();
+        }
+        return self::$instance;
+    }
+
+    private function __clone()
+    {
     }
 }
