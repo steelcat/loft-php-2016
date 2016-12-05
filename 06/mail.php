8d8e90e06d40352d859a3ebd9c60ed07502437ac
Steelcat <steelpuppy@gmail.com>
2016-12-05 20:15:53 +0300

    06 final

---
+++ 06/mail.php
@@ -0,0 +1,25 @@
+<?php
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer;
+//$mail->SMTPDebug = 3;
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'test@winfin.org';
+$mail->Password = 'abrakadabra';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('test@winfin.org', 'Mailer');
+$mail->addAddress('vs@zorca.org', 'Joe User');
+$mail->isHTML(true);
+$mail->Subject = 'Тема письма';
+$mail->Body    = 'Сообщение в письме';
+
+if (!$mail->send()) {
+    echo 'Сообщение не отправлено';
+    echo 'Ошибка: ' . $mail->ErrorInfo;
+} else {
+    echo 'Сообщение успешно отправлено';
+}
